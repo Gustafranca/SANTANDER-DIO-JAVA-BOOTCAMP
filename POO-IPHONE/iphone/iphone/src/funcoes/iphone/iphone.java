a94ef78820a6674aa1f5df00483baf991a17e79f
Gustavo França de Almeida <gustavofrancaa2001@gmail.com>
2024-06-17 16:16:21 -0300

    iphonproject

---
+++ POO-IPHONE/iphone/iphone/src/funcoes/iphone/iphone.java
@@ -0,0 +1,16 @@
+package funcoes.iphone;
+
+import funcoes.AparelhoTelefonico;
+import funcoes.NavegadorInternet;
+import funcoes.ReprodutorMusical;
+
+public class iphone {
+
+    public static void main(String[] args) {
+        AparelhoTelefonico at = new AparelhoTelefonico();
+        NavegadorInternet int = new NavegadorInternet();
+        ReprodutorMusical rm = new ReprodutorMusical();
+
+    }
+
+}
